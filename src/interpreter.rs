4631ce3f1d0aa62f4820cde09cd65e209f4bdc4e
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-01-07 17:28:03 +0000

    refactor: Refactor

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -5,24 +5,17 @@ pub fn execute(instructions: Vec<Instruction>) -> Vec<u8> {
     let mut pointer: isize = 0;
 
     let mut instruction_index = 0;
-    while instruction_index != instructions.len() {
-        let instruction = &instructions[instruction_index];
+    while let Some(instruction) = instructions.get(instruction_index) {
         match instruction {
             Instruction::Move(offset) => {
                 pointer += offset;
                 if pointer as usize >= memory.len() {
-                    memory.resize(memory.len() + *offset as usize, 0)
+                    memory.resize(pointer as usize + 1, 0)
                 }
-                instruction_index += 1;
             }
-            Instruction::Increment(increment) => {
-                memory[pointer as usize] += increment;
-                instruction_index += 1
-            }
-            Instruction::Decrement(decrement) => {
-                memory[pointer as usize] -= decrement;
-                instruction_index += 1
-            }
+            Instruction::Increment(increment) => memory[pointer as usize] += increment,
+            Instruction::Decrement(decrement) => memory[pointer as usize] -= decrement,
+
             Instruction::DecrementLoop(decrement) => {
                 let cell = unsafe { memory.get_unchecked_mut(pointer as usize) };
                 if *cell % *decrement == 0 {
@@ -30,7 +23,6 @@ pub fn execute(instructions: Vec<Instruction>) -> Vec<u8> {
                 } else {
                     panic!("Infinite loop detected")
                 }
-                instruction_index += 1
             }
             Instruction::IncrementLoop(increment) => {
                 let cell = unsafe { memory.get_unchecked_mut(pointer as usize) };
@@ -39,47 +31,37 @@ pub fn execute(instructions: Vec<Instruction>) -> Vec<u8> {
                 } else {
                     panic!("Infinite loop detected")
                 }
-                instruction_index += 1
             }
             Instruction::MoveLoop(offset) => {
                 while unsafe { *memory.get_unchecked(pointer as usize) } != 0 {
                     pointer += offset;
                     if pointer as usize >= memory.len() {
-                        memory.resize(memory.len() + *offset as usize, 0)
+                        memory.resize(pointer as usize + 1, 0)
                     }
                 }
-                instruction_index += 1
             }
             Instruction::LoopStart(loop_end) => {
                 let cell = unsafe { *memory.get_unchecked(pointer as usize) };
                 if cell == 0 {
-                    instruction_index = *loop_end
-                } else {
-                    instruction_index += 1
+                    instruction_index = *loop_end;
+                    continue;
                 }
             }
             Instruction::LoopEnd(loop_start) => {
                 let cell = unsafe { *memory.get_unchecked(pointer as usize) };
                 if cell != 0 {
-                    instruction_index = *loop_start
-                } else {
-                    instruction_index += 1
+                    instruction_index = *loop_start;
+                    continue;
                 }
             }
-            Instruction::Output => {
-                print!("{}", unsafe { *memory.get_unchecked(pointer as usize) }
-                    as char);
-
-                instruction_index += 1
-            }
+            Instruction::Output => print!("{}", unsafe { *memory.get_unchecked(pointer as usize) } as char),
             Instruction::Input => {
                 let mut input: [u8; 1] = [0; 1];
                 stdin().read_exact(&mut input).unwrap();
                 *memory.get_mut(pointer as usize).unwrap() = input[0];
-
-                instruction_index += 1
             }
         }
+        instruction_index += 1;
     }
 
     memory
