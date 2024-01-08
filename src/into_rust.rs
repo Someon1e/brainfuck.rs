c6360b8c44916050526057f0683f173781338485
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-01-08 19:21:51 +0000

    feat: Cleaner rust output

--- src/into_rust.rs
+++ src/into_rust.rs
@@ -2,7 +2,7 @@ use crate::compile::Instruction;
 
 pub fn to_rust(instructions: Vec<Instruction>) -> String {
     let mut code = vec![
-        String::from("let mut pointer: isize = 0;"),
+        String::from("let mut pointer: usize = 0;"),
         String::from("let mut memory: Vec<u8> = vec![0; 50];"),
     ];
 
@@ -12,22 +12,24 @@ pub fn to_rust(instructions: Vec<Instruction>) -> String {
                 if offset.is_positive() {
                 format!(
 "pointer += {offset};
-if pointer as usize >= memory.len() {{
-memory.resize(pointer as usize + 10, 0)
+if pointer >= memory.len() {{
+memory.resize(pointer + 10, 0)
 }}")
                 } else {
+                    // really hope it doesn't wrap around
+                    // TODO: fix above
                     format!("pointer -= {};", offset.abs())
                 }
             }
             Instruction::Increment(increment) => {
-                format!("memory[pointer as usize] += {increment};")
+                format!("memory[pointer] += {increment};")
             }
             Instruction::Decrement(decrement) => {
-                format!("memory[pointer as usize] -= {decrement};")
+                format!("memory[pointer] -= {decrement};")
             }
             Instruction::DecrementLoop(decrement) => {
                 format!(
-"let cell = unsafe {{ memory.get_unchecked_mut(pointer as usize) }};
+"let cell = unsafe {{ memory.get_unchecked_mut(pointer) }};
 if *cell % {decrement} == 0 {{
 *cell = 0
 }} else {{
@@ -35,7 +37,7 @@ panic!(\"Infinite loop detected\")
 }}")}
             Instruction::IncrementLoop(increment) => {
                 format!(
-"let cell = unsafe {{ memory.get_unchecked_mut(pointer as usize) }};
+"let cell = unsafe {{ memory.get_unchecked_mut(pointer) }};
 if *cell % {increment} == 0 {{
 *cell = 0
 }} else {{
@@ -45,27 +47,27 @@ panic!(\"Infinite loop detected\")
             Instruction::MoveLoop(offset) => {
                 if offset.is_positive() {
                     format!(
-"while unsafe {{ *memory.get_unchecked(pointer as usize) }} != 0 {{
+"while unsafe {{ *memory.get_unchecked(pointer) }} != 0 {{
 pointer += {offset};
-if pointer as usize >= memory.len() {{
-memory.resize(pointer as usize + 10, 0)
+if pointer >= memory.len() {{
+memory.resize(pointer + 10, 0)
 }}
 }}")
                     } else {
                     format!(
-"while unsafe {{ *memory.get_unchecked(pointer as usize) }} != 0 {{
+"while unsafe {{ *memory.get_unchecked(pointer) }} != 0 {{
 pointer -= {};
 }}", offset.abs())
                 }
             }
             Instruction::LoopStart(_loop_end) => {
-                r#"while unsafe { *memory.get_unchecked(pointer as usize) } != 0 {"#.to_owned()
+                r#"while unsafe { *memory.get_unchecked(pointer) } != 0 {"#.to_owned()
             }
             Instruction::LoopEnd(_loop_start) => {
                 r#"}"#.to_owned()
             }
             Instruction::Output => {
-                r#"print!("{}", unsafe { *memory.get_unchecked(pointer as usize) } as char);"#.to_owned()
+                r#"print!("{}", unsafe { *memory.get_unchecked(pointer) } as char);"#.to_owned()
             }
             Instruction::Input => {
                 unimplemented!()
