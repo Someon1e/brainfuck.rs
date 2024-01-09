d49d08fc6ac46a7d18062d50390d144b16275379
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-01-09 16:23:07 +0000

    feat: Cache cell in interpreter

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -6,6 +6,8 @@ pub fn execute(instructions: Vec<Instruction>) -> Vec<u8> {
     let mut memory: Vec<u8> = vec![0; 50];
     let mut pointer: isize = 0;
 
+    let mut cell = unsafe { memory.get_unchecked_mut(pointer as usize) };
+
     let mut instruction_index = 0;
     while let Some(instruction) = instructions.get(instruction_index) {
         match instruction {
@@ -14,12 +16,12 @@ pub fn execute(instructions: Vec<Instruction>) -> Vec<u8> {
                 if pointer as usize >= memory.len() {
                     memory.resize(pointer as usize + 10, 0)
                 }
+                cell = unsafe { memory.get_unchecked_mut(pointer as usize) };
             }
-            Instruction::Increment(increment) => memory[pointer as usize] += increment,
-            Instruction::Decrement(decrement) => memory[pointer as usize] -= decrement,
+            Instruction::Increment(increment) => *cell += increment,
+            Instruction::Decrement(decrement) => *cell -= decrement,
 
             Instruction::DecrementLoop(decrement) => {
-                let cell = unsafe { memory.get_unchecked_mut(pointer as usize) };
                 if *cell % *decrement == 0 {
                     *cell = 0
                 } else {
@@ -27,7 +29,6 @@ pub fn execute(instructions: Vec<Instruction>) -> Vec<u8> {
                 }
             }
             Instruction::IncrementLoop(increment) => {
-                let cell = unsafe { memory.get_unchecked_mut(pointer as usize) };
                 if *cell % *increment == 0 {
                     *cell = 0
                 } else {
@@ -35,32 +36,31 @@ pub fn execute(instructions: Vec<Instruction>) -> Vec<u8> {
                 }
             }
             Instruction::MoveLoop(offset) => {
-                while unsafe { *memory.get_unchecked(pointer as usize) } != 0 {
+                while *cell != 0 {
                     pointer += offset;
                     if pointer as usize >= memory.len() {
                         memory.resize(pointer as usize + 10, 0)
                     }
+                    cell = unsafe { memory.get_unchecked_mut(pointer as usize) };
                 }
             }
             Instruction::LoopStart(loop_end) => {
-                let cell = unsafe { *memory.get_unchecked(pointer as usize) };
-                if cell == 0 {
+                if *cell == 0 {
                     instruction_index = *loop_end;
                     continue;
                 }
             }
             Instruction::LoopEnd(loop_start) => {
-                let cell = unsafe { *memory.get_unchecked(pointer as usize) };
-                if cell != 0 {
+                if *cell != 0 {
                     instruction_index = *loop_start;
                     continue;
                 }
             }
-            Instruction::Output => write!(stdout, "{}", unsafe { *memory.get_unchecked(pointer as usize) } as char).unwrap(),
+            Instruction::Output => write!(stdout, "{}", *cell as char).unwrap(),
             Instruction::Input => {
                 let mut input: [u8; 1] = [0; 1];
                 stdin().read_exact(&mut input).unwrap();
-                *memory.get_mut(pointer as usize).unwrap() = input[0];
+                *cell = input[0];
             }
         }
         instruction_index += 1;
