a392d802cd28fe47dcb72c61dfaf6caa6e5c4fb5
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-01-14 11:41:44 +0000

    refactor: Refactor

--- src/compile.rs
+++ src/compile.rs
@@ -44,93 +44,103 @@ impl<'a> Compiler<'a> {
             compiling_instruction: CompilingInstruction::None,
         }
     }
-    fn push_compiling_instruction(&mut self) {
-        if self.compiling_instruction != CompilingInstruction::None {
-            self.instructions.push(match self.compiling_instruction {
-                CompilingInstruction::Move => Instruction::Move(self.value),
-                CompilingInstruction::Increment => {
-                    if self.value.is_positive() {
-                        Instruction::Increment(self.value as u8)
-                    } else {
-                        Instruction::Decrement(self.value.abs() as u8)
-                    }
-                }
-                CompilingInstruction::None => unreachable!(),
-            });
-            self.compiling_instruction = CompilingInstruction::None;
-            self.value = 0
+    fn compile_compiling_instruction(&mut self) {
+        if self.compiling_instruction == CompilingInstruction::None {
+            return;
         }
+        self.instructions.push(match self.compiling_instruction {
+            CompilingInstruction::Move => Instruction::Move(self.value),
+            CompilingInstruction::Increment => {
+                if self.value.is_positive() {
+                    Instruction::Increment(self.value as u8)
+                } else {
+                    Instruction::Decrement(self.value.abs() as u8)
+                }
+            }
+            CompilingInstruction::None => unreachable!(),
+        });
+        self.compiling_instruction = CompilingInstruction::None;
+        self.value = 0
     }
+    fn forward_backward(&mut self, token: Token) {
+        if self.compiling_instruction != CompilingInstruction::Move {
+            self.compile_compiling_instruction();
+            self.compiling_instruction = CompilingInstruction::Move;
+        }
+        self.value += if let Token::Forward = token { 1 } else { -1 };
+    }
+    fn increment_decrement(&mut self, token: Token) {
+        if self.compiling_instruction != CompilingInstruction::Increment {
+            self.compile_compiling_instruction();
+            self.compiling_instruction = CompilingInstruction::Increment;
+        }
+        self.value += if let Token::Increment = token { 1 } else { -1 };
+    }
+    fn start_loop(&mut self) {
+        self.loop_stack.push(self.instructions.len());
+        self.instructions.push(Instruction::LoopStart(0)) // temp 0
+    }
+    fn end_loop(&mut self) {
+        let loop_start = self.loop_stack.pop().expect("loop end without start"); // Index it should jump to in order to restart the loop
+        let loop_end = self.instructions.len(); // Index it should jump to in order to skip the loop
+
+        let replacement = if loop_end - loop_start - 1 == 1 {
+            // Only one type of instruction there
+            match self.instructions[loop_start + 1] {
+                Instruction::Decrement(decrement) => {
+                    self.instructions.remove(loop_start + 1);
+
+                    Instruction::DecrementLoop(decrement)
+                }
+                Instruction::Increment(increment) => {
+                    self.instructions.remove(loop_start + 1);
+
+                    Instruction::IncrementLoop(increment)
+                }
+                Instruction::Move(offset) => {
+                    self.instructions.remove(loop_start + 1);
+
+                    Instruction::MoveLoop(offset)
+                }
+                _ => {
+                    self.instructions.push(Instruction::LoopEnd(loop_start));
+                    Instruction::LoopStart(loop_end)
+                }
+            }
+        } else {
+            self.instructions.push(Instruction::LoopEnd(loop_start));
+            Instruction::LoopStart(loop_end)
+        };
+        self.instructions[loop_start] = replacement;
+    }
+
     pub fn compile(&mut self) -> &Vec<Instruction> {
         while let Some(token) = self.tokens.next() {
             match token {
-                Token::Forward | Token::Backward => {
-                    if self.compiling_instruction != CompilingInstruction::Move {
-                        self.push_compiling_instruction();
-                        self.compiling_instruction = CompilingInstruction::Move;
-                    }
-                    self.value += if let Token::Forward = token { 1 } else { -1 };
-                    continue;
-                }
-                Token::Increment | Token::Decrement => {
-                    if self.compiling_instruction != CompilingInstruction::Increment {
-                        self.push_compiling_instruction();
-                        self.compiling_instruction = CompilingInstruction::Increment;
-                    }
-                    self.value += if let Token::Increment = token { 1 } else { -1 };
-                    continue;
-                }
-                Token::Comment => continue,
-                _ => {}
-            }
+                Token::Forward | Token::Backward => self.forward_backward(token),
+                Token::Increment | Token::Decrement => self.increment_decrement(token),
+                Token::Comment => {}
 
-            self.push_compiling_instruction();
-            match token {
                 Token::LoopStart => {
-                    self.loop_stack.push(self.instructions.len());
-                    self.instructions.push(Instruction::LoopStart(0)) // temp 0
+                    self.compile_compiling_instruction();
+                    self.start_loop()
                 }
                 Token::LoopEnd => {
-                    let loop_start = self.loop_stack.pop().expect("loop end without start"); // Index it should jump to in order to restart the loop
-                    let loop_end = self.instructions.len(); // Index it should jump to in order to skip the loop
-
-                    let replacement = if loop_end - loop_start - 1 == 1 {
-                        // Only one type of instruction there
-                        match self.instructions[loop_start + 1] {
-                            Instruction::Decrement(decrement) => {
-                                self.instructions.remove(loop_start + 1);
-
-                                Instruction::DecrementLoop(decrement)
-                            }
-                            Instruction::Increment(increment) => {
-                                self.instructions.remove(loop_start + 1);
-
-                                Instruction::IncrementLoop(increment)
-                            }
-                            Instruction::Move(offset) => {
-                                self.instructions.remove(loop_start + 1);
-
-                                Instruction::MoveLoop(offset)
-                            }
-                            _ => {
-                                self.instructions.push(Instruction::LoopEnd(loop_start));
-
-                                Instruction::LoopStart(loop_end)
-                            }
-                        }
-                    } else {
-                        self.instructions.push(Instruction::LoopEnd(loop_start));
-                        Instruction::LoopStart(loop_end)
-                    };
-                    self.instructions[loop_start] = replacement;
+                    self.compile_compiling_instruction();
+                    self.end_loop()
                 }
-                Token::Input => self.instructions.push(Instruction::Input),
-                Token::Output => self.instructions.push(Instruction::Output),
-                _ => unreachable!(),
+                Token::Input => {
+                    self.compile_compiling_instruction();
+                    self.instructions.push(Instruction::Input)
+                }
+                Token::Output => {
+                    self.compile_compiling_instruction();
+                    self.instructions.push(Instruction::Output)
+                }
             }
         }
 
-        self.push_compiling_instruction();
+        self.compile_compiling_instruction();
 
         if !self.loop_stack.is_empty() {
             panic!("Unclosed loop");
